b0c5844cd88524742d0c0dbc2b5a5a2688ba3719
drcmomo <moise_dormeus@hotmail.com>
2016-04-21 20:15:44 +0200

    Added files via upload

---
+++ IFibonacci.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTechniqueFibonacci
+{
+   public interface IFibonacci
+    {
+        string XmlToJson(string xml);
+
+        int Fibonnacci(int n);
+    }
+}
---
+++ ServiceFibonacci.asmx.cs
@@ -0,0 +1,182 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+using System.Xml;
+
+namespace TestTechniqueFibonacci
+{
+    /// <summary>
+    /// Description résumée de ServiceFibonacci
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Pour autoriser l'appel de ce service Web depuis un script à l'aide d'ASP.NET AJAX, supprimez les marques de commentaire de la ligne suivante. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ServiceFibonacci : IFibonacci
+    {
+
+        [WebMethod]
+        public int Fibonnacci(int n)
+        {
+            int value1 = 0;
+            int value2 = 1;
+            if (n < 0 || n > 100 || n == 0)
+                return -1;
+
+            for (int i = 1; i < n; i++)
+            {
+                int temp = value1;
+                value1 = value2;
+                value2 = temp + value2;
+            }
+            return value1;
+        }
+
+        [WebMethod]
+
+        public string XmlToJson(string xml)
+        {
+
+          
+            string removeParenthese = xml.Replace("(", String.Empty);
+            string removeParenthese1 = removeParenthese.Replace(")", String.Empty);          
+            object yourOjbect = new JavaScriptSerializer().DeserializeObject(removeParenthese1);
+            var test = string.Concat("<? xml version = '1.0'>", yourOjbect.ToString());     
+            try
+            {  
+            XmlDocument document = new XmlDocument();
+            document.LoadXml(yourOjbect.ToString());
+            return XmlToJSON(document);
+            }
+            catch(Exception ex)
+            {
+                return "Bad Xml format";
+            }
+
+        }
+        public static string XmlToJSON(XmlDocument xmlDoc)
+        {
+            StringBuilder sbJSON = new StringBuilder();
+            sbJSON.Append("{ ");
+            XmlToJSONnode(sbJSON, xmlDoc.DocumentElement, true);
+            sbJSON.Append("}");
+            return sbJSON.ToString();
+        }
+        private static void XmlToJSONnode(StringBuilder sbJSON, XmlElement node, bool showNodeName)
+        {
+            if (showNodeName)
+                sbJSON.Append("\"" + SafeJSON(node.Name) + "\": ");
+            sbJSON.Append("{");
+           
+            SortedList<string, object> childNodeNames = new SortedList<string, object>();
+
+            //  Add in all node attributes
+            if (node.Attributes != null)
+                foreach (XmlAttribute attr in node.Attributes)
+                    StoreChildNode(childNodeNames, attr.Name, attr.InnerText);
+
+            //  Add in all nodes
+            foreach (XmlNode cnode in node.ChildNodes)
+            {
+                if (cnode is XmlText)
+                    StoreChildNode(childNodeNames, "value", cnode.InnerText);
+                else if (cnode is XmlElement)
+                    StoreChildNode(childNodeNames, cnode.Name, cnode);
+            }
+
+            // Now output all stored info
+            foreach (string childname in childNodeNames.Keys)
+            {
+                List<object> alChild = (List<object>)childNodeNames[childname];
+                if (alChild.Count == 1)
+                    OutputNode(childname, alChild[0], sbJSON, true);
+                else
+                {
+                    sbJSON.Append(" \"" + SafeJSON(childname) + "\": [ ");
+                    foreach (object Child in alChild)
+                        OutputNode(childname, Child, sbJSON, false);
+                    sbJSON.Remove(sbJSON.Length - 2, 2);
+                    sbJSON.Append(" ], ");
+                }
+            }
+            sbJSON.Remove(sbJSON.Length - 2, 2);
+            sbJSON.Append(" }");
+        }
+        private static void StoreChildNode(SortedList<string, object> childNodeNames, string nodeName, object nodeValue)
+        {
+            if (nodeValue is XmlElement)
+            {
+                XmlNode cnode = (XmlNode)nodeValue;
+                if (cnode.Attributes.Count == 0)
+                {
+                    XmlNodeList children = cnode.ChildNodes;
+                    if (children.Count == 0)
+                        nodeValue = null;
+                    else if (children.Count == 1 && (children[0] is XmlText))
+                        nodeValue = ((XmlText)(children[0])).InnerText;
+                }
+            }
+            List<object> ValuesAL;
+
+            if (childNodeNames.ContainsKey(nodeName))
+            {
+                ValuesAL = (List<object>)childNodeNames[nodeName];
+            }
+            else
+            {
+                ValuesAL = new List<object>();
+                childNodeNames[nodeName] = ValuesAL;
+            }
+            ValuesAL.Add(nodeValue);
+        }
+        private static void OutputNode(string childname, object alChild, StringBuilder sbJSON, bool showNodeName)
+        {
+            if (alChild == null)
+            {
+                if (showNodeName)
+                    sbJSON.Append("\"" + SafeJSON(childname) + "\": ");
+                sbJSON.Append("null");
+            }
+            else if (alChild is string)
+            {
+                if (showNodeName)
+                    sbJSON.Append("\"" + SafeJSON(childname) + "\": ");
+                string sChild = (string)alChild;
+                sChild = sChild.Trim();
+                sbJSON.Append("\"" + SafeJSON(sChild) + "\"");
+            }
+            else
+                XmlToJSONnode(sbJSON, (XmlElement)alChild, showNodeName);
+            sbJSON.Append(", ");
+        }
+
+        // Make a string safe for JSON
+        private static string SafeJSON(string sIn)
+        {
+            StringBuilder sbOut = new StringBuilder(sIn.Length);
+            foreach (char ch in sIn)
+            {
+                if (Char.IsControl(ch) || ch == '\'')
+                {
+                    int ich = (int)ch;
+                    sbOut.Append(@"\u" + ich.ToString("x4"));
+                    continue;
+                }
+                else if (ch == '\"' || ch == '\\' || ch == '/')
+                {
+                    sbOut.Append('\\');
+                }
+                sbOut.Append(ch);
+            }
+            return sbOut.ToString();
+        }
+    }
+}
